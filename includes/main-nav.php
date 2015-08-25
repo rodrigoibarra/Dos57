5542a0cb930c824114310f6a284877bc59501234
rodrigoibarra <hola@rodrigo-ibarra.com>
2015-08-25 13:12:20 -0500

    did some stuff with nav

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -1,5 +1,6 @@
 <nav id="header">
-  <img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" />
+  <img  src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" />
+  <a href=""> <?php include("includes/dos57.svg"); ?>  </a>
   <a href="/">Home</a>
   <a href="/portafolio">Portafolio</a>
 </nav>
