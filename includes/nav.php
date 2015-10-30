a6505c6031fc125cd6d32dd6915b8bf09ed70a03
rodrigoibarra <hola@rodrigo-ibarra.com>
2015-10-30 07:48:03 -0600

    added home link to logo, and fixed a bug on that, also added a contact link for the main nav and cant remember what else

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <nav id="header" class="nav-up">
-  <img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" />
-  <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" />
+  <a href="/"><img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" /></a>
+  <a href="/"> <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" /></a>
   <a href="#nosotros">Nosotros</a>
   <a href="#portafolio">Portafolio</a>
   <a href="#contacto">Contacto</a>
