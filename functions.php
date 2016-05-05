14de8a6f13ec9acb07d5d56a8d0449d4eca7b6fe
rodrigoibarra <hola@rodrigo-ibarra.com>
2016-05-04 21:19:26 -0500

    this commit updates the repo with some fixes that were made comando style directly on server

--- functions.php
+++ functions.php
@@ -5,4 +5,12 @@ function my_jquery_enqueue() {
    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
 }
+function wpb_imagelink_setup() {
+	$image_set = get_option( 'image_default_link_type' );
+
+	if ($image_set !== 'none') {
+		update_option('image_default_link_type', 'none');
+	}
+}
+add_action('admin_init', 'wpb_imagelink_setup', 10);
 ?>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Noticia+Text:700' rel='stylesheet' type='text/css'>
       <?php wp_head(); ?>
    </head>
   <body <?php body_class(); ?>>
