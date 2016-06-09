7f1700bc47f2572ba90c705878858fe84c979789
rodrigoibarra <hola@rodrigo-ibarra.com>
2016-06-08 23:39:15 -0500

    cambie el nav para usar flexbox... resuelve muchos problemas... espero funcione siempre

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -1,4 +1,4 @@
-<nav id="header">
+<nav id="header" class="flex-nav">
   <a href="/"><img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" /></a>
   <a href="/"> <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" /></a>
   <a href="/">Home</a>
--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,4 @@
-<nav id="header" class="nav-up">
+<nav id="header" class="nav-up flex-nav">
   <a href="/"><img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" /></a>
   <a href="/"> <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" /></a>
   <a href="#nosotros">Nosotros</a>
