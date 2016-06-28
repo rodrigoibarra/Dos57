30106212f06f732b584a8f1d69cc2d51707a90fd
rodrigoibarra <hola@rodrigo-ibarra.com>
2016-06-28 17:32:35 -0500

    flex nav is done, works way better than the first itteration

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -4,10 +4,7 @@
   <a href="/">Home</a>
   <a href="/portafolio">Portafolio</a>
   <a href="/#contacto">Contacto</a>
-  <form class="nav-form" method="get" action="<?php bloginfo('url'); ?>">
-    <fieldset>
-    <input type="text" name="category_name" value="" placeholder="Busca por categoría" maxlength="50" required="required" />
-    <button type="submit">Buscar</button>
-    </fieldset>
-  </form>
+
+  <?php echo do_shortcode( '[searchandfilter fields="category" class="nav-form" search_placeholder="categorias" submit_label="Buscar" taxonomies="post_tag"]' ); ?>
+
 </nav>
--- includes/nav.php
+++ includes/nav.php
@@ -4,10 +4,5 @@
   <a href="#nosotros">Nosotros</a>
   <a href="#portafolio">Portafolio</a>
   <a href="#contacto">Contacto</a>
-  <form class="nav-form" method="get" action="<?php bloginfo('url'); ?>">
-    <fieldset>
-    <input type="text" name="category_name" value="" placeholder="Busca por categoría" maxlength="50" required="required" />
-    <button type="submit">Buscar</button>
-    </fieldset>
-  </form>
+  <?php echo do_shortcode( '[searchandfilter fields="search" class="nav-form" search_placeholder="categorias" submit_label="Buscar" taxonomies="post_tag"]' ); ?>
 </nav>
