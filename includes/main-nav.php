5de61ef4c4bfff0044e036cb604185bb343c380f
rodrigoibarra <hola@rodrigo-ibarra.com>
2016-06-29 11:23:43 -0500

    some more flexing, fixes navigation the right way, it also got reorganized

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -1,13 +1,15 @@
-<nav id="header" class="flex-nav">
-  <a href="/"><img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" /></a>
-  <a href="/"> <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" /></a>
-  <a href="/">Home</a>
-  <a href="/portafolio">Portafolio</a>
-  <a href="/#contacto">Contacto</a>
-  <a id="toggle">Categorias</a>
+<nav id="header">
+  <div class="flex-nav">
+    <a href="/"><img src="<?php bloginfo('template_url'); ?>/build/images/logo-h.png" class="logo-h" alt="Dos Cincuentaysiete" /></a>
+    <a href="/"> <img src="<?php bloginfo('template_url'); ?>/build/images/Dos57-resp.png" class="resp-logo" alt="Dos Cincuentaysiete" /></a>
+    <a href="/">Home</a>
+    <a href="/portafolio">Portafolio</a>
+    <a href="/#contacto">Contacto</a>
+    <a id="toggle">Categorias</a>
+  </div>
+  <div class="cat-nav cat-nav-hide ">
+    <?php wp_list_categories( array(
+        'title_li' => ''
+    ) ); ?>
+  </div>
 </nav>
-<div class="cat-nav cat-nav-hide ">
-  <?php wp_list_categories( array(
-      'title_li' => ''
-  ) ); ?>
-</div>
--- single.php
+++ single.php
@@ -1,19 +1,20 @@
 <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 <?php include("includes/main-nav.php"); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 <div class="hero" style="background:linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)), linear-gradient(rgba(0, 0, 0, 0.10), rgba(0, 0, 0, 0.90)), url(<?php the_field('featured'); ?>);">
-  <div class="intro">
     <h1 class="titulo"><?php the_title(); ?></h1>
-  </div>
 
 </div>
 
     <div class="wrap cf">
       <section class="single-post cf">
-        <p><?php the_field('intro')?></p>
-          <div class="galeria-flex">
+        <div class="intro">
+          <p><?php the_field('intro')?></p>
+        </div>
+        <div class="galeria-flex">
             <?php the_content() ?>
-          </div>
+        </div>
       </section>
 
       <div class="post-nav">
